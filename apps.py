4bcef38b84560b396776bd3e48ae5681ba34ecec
Ubuntu <herlendy.sanchez11@gmail.com>
2023-03-02 01:02:41 +0000

    Funcion lambda

---
+++ apps.py
@@ -0,0 +1,14 @@
+import json
+from urllib.request import urlopen
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    with urlopen("https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario") as response:
+        body = response.read()
+    client = boto3.client("s3")
+    client.put_object(Body = body, Bucket = "dolar-raw-2023", Key = "dolar_timestamp.txt")
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda!")
+    }
