1c193064acd90827089e93d8c775515516234755
Ubuntu <herlendy.sanchez11@gmail.com>
2023-03-02 02:11:21 +0000

    Actualizacion archivos

--- apps.py
+++ apps.py
@@ -6,6 +6,7 @@ def f(event, context):
     # TODO implement
     with urlopen("https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario") as response:
         body = response.read()
+        
     client = boto3.client("s3")
     client.put_object(Body = body, Bucket = "dolar-raw-2023", Key = "dolar_timestamp.txt")
     return {
